68b2e7e1429c9d490cafcdc2379b5611c81e1e07
MAXSCRIPT <lukuanenko@gmail.com>
2024-10-03 16:04:43 +0300

    1.0.2

---
+++ chrome.bat
@@ -0,0 +1,28 @@
+:: Chrome
+:: 1.0.0
+:: Vasyl Lukianenko 
+:: 3DGROUND
+:: https://3dground.net
+
+echo off
+cls
+
+set input=%1
+set arg1=
+set arg2=
+
+for /f "tokens=1,2 delims=;" %%I in (%input%) do (
+	set arg1=%%I
+	set arg2=%%J
+)
+
+taskkill /f /im chrome.exe
+start chrome.exe --incognito --start-fullscreen "%arg1%"
+
+timeout /t 10 /nobreak > nul
+
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds; $bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height; $graphics = [System.Drawing.Graphics]::FromImage($bitmap); $graphics.CopyFromScreen($screen.Location, [System.Drawing.Point]::Empty, $screen.Size); $bitmap.Save('%arg2%'); $graphics.Dispose(); $bitmap.Dispose();"
+
+taskkill /f /im chrome.exe
+
+exit 0
