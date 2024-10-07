9b303109a2b43f205cb0166ff1987a29cd4fc7f8
MAXSCRIPT <lukuanenko@gmail.com>
2024-10-07 15:35:11 +0300

    1.0.4

--- blender-screenshot.bat
+++ blender-screenshot.bat
@@ -21,21 +21,15 @@ for /f "tokens=1,2,3,4 delims=;" %%i in (%input%) do (
 	set scr=%%l
 )
 
-:::!!!
-set blender_alt=c:\Program Files\WindowsApps\BlenderFoundation.Blender3.6LTS_3.6.16.0_x64__ppwjx1n5r4v9t\Blender\blender-launcher.exe
-
-if exist "%blender_alt%" (
-	set blender=%blender_alt%
-)
 
 taskkill /f /im blender.exe
 
-"%blender%" "%file%" -P "%script%"
+start "" "%blender%" "%file%" -P "%script%"
 
-timeout /t 10 /nobreak > nul
+timeout /t 25 /nobreak > nul
 
 powershell -command "Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds; $bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height; $graphics = [System.Drawing.Graphics]::FromImage($bitmap); $graphics.CopyFromScreen($screen.Location, [System.Drawing.Point]::Empty, $screen.Size); $bitmap.Save('%scr%'); $graphics.Dispose(); $bitmap.Dispose();"
 
 taskkill /f /im blender.exe
 
-rem exit 0
+exit 0
--- blender-zoom-extends.py
+++ blender-zoom-extends.py
@@ -1,15 +1,16 @@
 import bpy
 
-context = bpy.context
+def get_view3d():
+    for window_manager in bpy.data.window_managers:
+        for window in window_manager.windows:
+            for area in window.screen.areas:
+                if area.type == 'VIEW_3D':
+                    for region in area.regions:
+                        if region.type == 'WINDOW':
+                            return dict(window=window, workspace=window.workspace, screen=window.screen, area=area, region=region)
 
-context.preferences.view.show_splash = False
+    raise Exception('View3D context not found.')
 
-for area in context.screen.areas:
-    if area.type == 'VIEW_3D':
-        override = context.copy()
-        override['area'] = area
-        
-        for region in area.regions:
-            if region.type == 'WINDOW':
-                override['region'] = region
-                bpy.ops.view3d.view_all(override, center=True)
+
+with bpy.context.temp_override(**get_view3d()):
+    bpy.ops.view3d.view_all(center=False)
