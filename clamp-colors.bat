135819709311facdf806fad963075e99fce853b7
MAXSCRIPT <lukuanenko@gmail.com>
2024-10-30 17:02:02 +0200

    1.2.1

---
+++ clamp-colors.bat
@@ -0,0 +1,22 @@
+:: Clamp Colors
+:: 1.0.0
+:: Vasyl Lukianenko 
+:: 3DGROUND
+:: https://3dground.net
+
+echo off
+cls
+
+set input=%1
+set in=
+set out=
+
+
+for /f "tokens=1,2,3,4,5 delims=;" %%i in (%input%) do (
+	set in=%%i
+    set out=%%j
+)
+
+convert.exe "%in%" -level -2%%,102%% "%out%"
+
+::exit 0
