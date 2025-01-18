fa23dbe0ad2bad853f0d341d5871e3bbb63714c7
MAXSCRIPT <lukuanenko@gmail.com>
2025-01-18 16:51:46 +0200

    1.5.2
    Skp

--- sketchup-convert.bat
+++ sketchup-convert.bat
@@ -8,33 +8,47 @@ echo off
 
 cls
 
+set "currentDate=%DATE%"
+set "currentTime=%TIME%"
+set "timestamp=%currentDate%_%currentTime%"
+set "timestamp=%timestamp::=-%"
+set "timestamp=%timestamp: =_%"
+set "timestamp=%timestamp:/=-%"
+set "timestamp=%timestamp:,=-%"
+
 set input=%1
 set sketchup=
-set vrscene=
+set glb=
 set skp=
-set script=c:\temp\sketchup_saver.rb
+set script=c:\temp\sketchup_ruby_%timestamp%.rb
 
 for /f "tokens=1,2,3 delims=;" %%i in (%input%) do (
 	set sketchup=%%i
-	set vrscene=%%j
+	set glb=%%j
 	set skp=%%k
 )
 
+::echo File: %skp%>ruby_log.txt
+
 ::Write Ruby Script
+
 echo begin>%script%
 echo model = Sketchup.active_model>>%script%
-echo model.import("%vrscene%", show_summary: false)>>%script%
+echo model.import("%glb%", show_summary: false)>>%script%
 ::echo view = model.active_view>>%script%
 ::echo view.zoom_extents>>%script%
 echo model.save("%skp%")>>%script%
-echo Sketchup.quit>>%script%
+echo spawn("taskkill /F /IM SketchUp.exe")>>%script%
+::echo Sketchup.quit>>%script%
 echo end>>%script%
 
 ::Run Sketchup
 taskkill /F /IM SketchUp.exe
-"%sketchup%" "C:\temp\template.skp" -RubyStartup "%script%" > log.txt
+"%sketchup%" "C:\temp\template.skp" -RubyStartup "%script%" 
+::>> ruby_log.txt
+taskkill /F /IM SketchUp.exe
 taskkill /F /IM BsSndRpt64.exe
 
-del "%script%" /f /q
+del "%script%" /q > nul
 
 ::exit 0
