c3f6fa6711e6d8c2af5b8ab2cb900893e1362443
MAXSCRIPT <lukuanenko@gmail.com>
2025-01-23 12:29:04 +0200

    1.5.4

--- gltfpack.bat
+++ gltfpack.bat
@@ -10,13 +10,17 @@ cls
 set input=%1
 set gltf=
 set glb=
+set glb2=
 
 for /f "tokens=1,2,3 delims=;" %%i in (%input%) do (
 	set gltf=%%i
 	set glb=%%j
+	set glb2=%%k
 )
 
 gltfpack.exe -noq -i "%gltf%" -o "%glb%"
+gltfpack.exe -cc -vtf -vnf -vpf -vc 16 -vn 16 -vp 16 -vt 16 -tc -tl 2048 -i "%gltf%" -o "%glb2%"
+
 
 for %%a in ("%gltf%") do set "folder=%%~dpa"
 timeout /t 2 /nobreak > nul & rmdir /s /q "%folder%"
